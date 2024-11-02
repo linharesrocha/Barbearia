e2f5e5794159045f8fefebd7fd1201d581b01bc5
linharesrocha <guilhermelinharesr@gmail.com>
2024-11-02 02:39:22 -0300

    add entity framework core configuration

--- Barbearia.API/Program.cs
+++ Barbearia.API/Program.cs
@@ -1,11 +1,16 @@
+using Barbearia.Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Database
+builder.Services.AddDbContext<BarbeariaDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
---
+++ Barbearia.Infrastructure/Context/BarbeariaDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Barbearia.Infrastructure.Context
+{
+    public class BarbeariaDbContext : DbContext
+    {
+        public BarbeariaDbContext(DbContextOptions<BarbeariaDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+        }
+    }
+}
